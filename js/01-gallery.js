da18b243be775a113f1b38b4cda4924431e09cc9
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-08 19:34:57 +0200

    Создание и рендер разметки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки
+
+const galleryElements = document.querySelector('.gallery');
+const createGalleryItemMarkup = ({preview, original, description}) =>
+`
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+
+const makeGalleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+
+galleryElements.innerHTML = makeGalleryMarkup;
+// console.log(galleryItems);
