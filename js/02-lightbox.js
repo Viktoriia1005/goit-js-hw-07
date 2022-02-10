be6440f24ea97e54a52873179f0cc8cc54c831fd
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-02-10 14:10:20 +0200

    task-2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных
+
+const galleryList = document.querySelector('.gallery');
+const createGalleryItemMarkup = ({preview, original, description}) =>
+`
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+`;
+const createGalleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryList.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+// Использование библиотеки
+
+let upgradeGallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionPosition: 'bottom',
+    captionsData: "alt",
+    captionDelay: 250,
+});
